3afa5e220a7b5122341f227a089b7d371ca8d326
Anar Bayramov <bayramovanar1703@gmail.com>
2022-10-02 18:18:17 +0400

    mobile version

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-/* mobile version menu appear on Y position */ 
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
 
+   /* mobile version menu appear on Y position */  
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
+
+/* mobile version menu hide */ 
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* remove menu bar on mobile */ 
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
