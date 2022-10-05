09458d5d779468118de67ce8734add55ea59b496
Anar Bayramov <bayramovanar1703@gmail.com>
2022-10-06 00:07:53 +0400

    portfolio

--- script.js
+++ script.js
@@ -68,3 +68,47 @@ tabs.forEach(tab =>{
         tab.classList.add('qualification__active')
     })
 })
+
+
+
+
+/* Service model being toggled */ 
+
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'), 
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i)=>{
+   modalBtn.addEventListener('click', ()=>{
+    modal(i)
+   })
+})
+
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+       modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+       })
+    })
+})
+
+
+/* Swiper */ 
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true, 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true, 
+    },
+  });
